e3b0d26afffa8226a7d7979fb05d785950705a13
Saeef-47 <saeefmohammed8@gmail.com>
2025-08-08 21:19:51 +0300

    Add files via upload

---
+++ Name_Capitalization_Formating.sql
@@ -0,0 +1,7 @@
+CREATE TABLE "Names" ("Name" VARCHAR(100));
+INSERT INTO "Names" ("Name") VALUES
+('rAVI kUMAR'),
+('priya sharma'),
+('amit PATEL'),
+('NEHA gupta');
+SELECT INITCAP("Name") AS output FROM "Names";
